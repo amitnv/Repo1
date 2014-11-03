b2343a757fa88adda67843a6f6f1e1556b3e38f9
Amit Vaidya <amitvaidhya@gmail.com>
2014-11-03 22:21:35 +0530

    Update myName.js

--- myName.js
+++ myName.js
@@ -1,11 +1,14 @@
 var myName = "Amit";
 var status="";
 
-if(myName.length >=7)
+if(myName.length >=15)
 {
-  status="You have a long name!";
+  status="You have a very long name!";
 }
-else
+else if(myName.length >=7)
+{
+    status="You have a long name!";
+}
 {
   status="You have a short name!";
 }  
