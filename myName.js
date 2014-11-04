f24537ba0a00694c5c52a98b3ad51eea31757ef2
Amit Vaidya <amitvaidhya@gmail.com>
2014-11-04 17:17:15 +0530

    Update myName.js

--- myName.js
+++ myName.js
@@ -1,14 +1,1 @@
-var myName = "Amit";
-var status=""; /* we can also use "var status;"(without quotes) here */
 
-if(myName.length >=15)
-{
-  status="You have a very long name!";
-}
-else if(myName.length >=7)
-{
-    status="You have a long name!";
-}
-{
-  status="You have a short name!";
-}  
